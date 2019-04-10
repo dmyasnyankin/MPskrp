6c51f0c87c777576b944cfb247303345d6df621f
Dennis Myasnyankin <dennis415@live.com>
2019-04-09 23:17:24 -0700

    Click on CX hamburger

--- CXskrp/index.js
+++ CXskrp/index.js
@@ -11,20 +11,30 @@ const puppeteer = require('puppeteer');
     await page.goto('https://google.com');
     await page.type('input[class="gLFyf gsfi"]', 'Cathay Pacific Airlines', {delay: 25}); // Types slower, like a user
     await page.keyboard.press('Enter');
+    console.log('Searching for CX...');
     await page.waitForNavigation();
     
     // Click first DL link spotted
     // await page.click('cite[class="www.cathaypacific.com/"]');
     await page.click('div[class="ads-visurl"]');
-    await page.waitForNavigation();
+    await page.waitFor(3000);
+    console.log('CX loaded...');
 
     // LIKELY TO CHANGE
     // Copied selector path in order to find My Trips 
     // await page.click('body > app-root > app-home > ngc-global-nav > header > div > div.widget-container-lg.nav-content-outlet.bg-dark.main-container.collapse-widget > app-links-content-mobile-container > div > ul > li:nth-child(3) > a');
     // await page.click('div[class="parbase home-tabs tabs-wrapper holder-fluid"] > ul[class="tabs"] > li]')[2];
     // await page.click('ul[class="tabs"] li a[id="#manage-booking-tab"]');
-    await page.waitFor('a[href="https://www.cathaypacific.com/mb/#/en_US/login"]');
-    await page.click('a[href="https://www.cathaypacific.com/mb/#/en_US/login"]');
+    
+    // await page.waitFor('a[href="https://www.cathaypacific.com/mb/#/en_US/login"]');
+    // await page.click('div[class="header-top-row"] i[class="icon icon-menu"]');
+    await page.click('i[class="icon icon-menu"]');
+
+    console.log('Click on Hamburger Icon');
+
+    // await page.click('a[href="https://www.cathaypacific.com/mb/#/en_US/login"]');
+
+
     // FAILED ATTEMPTS at finding selector above:
 
     // Type in Conf # and PAX name
