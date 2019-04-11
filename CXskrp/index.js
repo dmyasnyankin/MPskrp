74014b966e6ac5559100c18a41e68e8ca1d2de32
Dennis Myasnyankin <dennis415@live.com>
2019-04-11 14:01:23 -0700

    CX enter booking info

--- CXskrp/index.js
+++ CXskrp/index.js
@@ -29,18 +29,40 @@ const puppeteer = require('puppeteer');
     // await page.waitFor('a[href="https://www.cathaypacific.com/mb/#/en_US/login"]');
     // await page.click('div[class="header-top-row"] i[class="icon icon-menu"]');
     await page.click('i[class="icon icon-menu"]');
-
+    
     console.log('Click on Hamburger Icon');
+    
+    // await page.click('ul[class="navigation-list"]')[2];
 
-    // await page.click('a[href="https://www.cathaypacific.com/mb/#/en_US/login"]');
+    
+    await page.waitFor(3000);
+    
+    // FAILED ATTEMPTS at finding manage booking in hamburger
+    // await page.click('div[class="side-menu-panel"] div[class="side-menu-navigation"] ul[class="navigation-items"] li[class="navigation-item"]')[2];
+    // WORKING ATTEMPT via copying selector path
+    // await page.click('body > header > div.parbase.header > div.cx-header-content.full-header.header-grey-bottom-box-shadow > div > div.side-menu-panel.show-by-opacity > div.side-menu-content > div.side-menu-navigation > ul > li:nth-child(2)');
+    // SHORTENED WORKING ATTEMPT
+    await page.click('div.side-menu-navigation > ul > li:nth-child(2)');
+    console.log('Clicked on Manage Tab');
 
+    // WA 
+    // await page.click('body > header > div.parbase.header > div.cx-header-content.full-header.header-grey-bottom-box-shadow > div > div.side-menu-panel.show-by-opacity > div.side-menu-content.show-child-content > div.side-menu-navigation > ul > li:nth-child(2) > div > div > div > div:nth-child(1) > div > div.navigation-header-title-des > div.parsys.contentpar > div:nth-child(2) > a')
+    // SA
+    await page.click('ul > li:nth-child(2) div:nth-child(1) div:nth-child(2) > a')
+    await page.waitForNavigation();
+    console.log('Clicked on Manage My Booking');
 
+    // WA
+    // await page.click('#online-checkin-tab > span');
+    // SA
+    await page.click('#online-checkin-tab');
     // FAILED ATTEMPTS at finding selector above:
 
     // Type in Conf # and PAX name
-    // await page.type('input[id="confirmationNo"]', 'B6969B', {delay: 100});
-    // await page.type('input[id="firstName"]', 'Joe', {delay: 100});
-    // await page.type('input[id="lastName"]', 'Blow', {delay: 100});
+    await page.type('input[id="givenName"]', 'Piska', {delay: 100});
+    await page.type('input[id="familyName"]', 'Chlen', {delay: 100});
+    await page.type('#rloc-and-eticket', 'ILXILX', {delay: 100});
+    
     // Click to enter PAX info
     // await page.keyboard.press('Enter');
 
@@ -48,5 +70,5 @@ const puppeteer = require('puppeteer');
     // await page.waitForNavigation();
 
     debugger;
-    // await browser.close();
+    await browser.close();
 })();
