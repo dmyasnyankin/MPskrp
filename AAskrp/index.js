448d90ea35014cc0cef8dfe506854653370c29f2
Dennis Myasnyankin <dennis415@live.com>
2019-07-30 17:47:55 -0700

    Initialize AA skrp entrance

---
+++ AAskrp/index.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+        // devtools: true
+    });
+    // Enter google and type in the Airline Search
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
+    await page.type('input[class="gLFyf gsfi"]', 'American Airlines', {delay: 25}); // Types slower, like a user
+    await page.keyboard.press('Enter');
+    console.log('Searching for AA...');
+    await page.waitForNavigation();
+    await page.waitFor(5000);
+    
+    await page.click('a[href="https://www.aa.com/homePage.do"]');
+    await page.waitFor(10000);
+    console.log('AA loaded...');
+
+    await page.click('a[href="#aa-viewReservations"]');
+    console.log('Clicked on View Reservations...');
+    await page.waitFor(5000);
+
+    await page.type('form[id="findReservationForm"] input[name="firstName"]', 'Vladimir', {delay: 25});
+    await page.type('form[id="findReservationForm"] input[name="lastName"]', 'Putin', {delay: 25});
+    await page.type('form[id="findReservationForm"] input[name="recordLocator"]', 'PYCC1R', {delay: 25});
+    console.log('Typed in PAX deets...')
+    // Type in Conf # and PAX name
+    // await page.type('input[id="givenName"]', 'Chandler', {delay: 100});
+    // await page.type('input[id="familyName"]', 'Bing', {delay: 100});
+    // await page.type('#rloc-and-eticket', 'FRND2Z', {delay: 100});
+    debugger
+    await browser.close();
+})();
